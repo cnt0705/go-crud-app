9820affeadfadbca5b64424b9b0fea5f5758f69c
Chinatsu Kato <cnt.tkhs@gmail.com>
2021-06-09 09:52:50 +0900

    Read todo by user

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 func main() {
 	fmt.Println(models.Db)
 
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
-	// u.Password = "testpassword"
-	// fmt.Println(u)
-	// u.CreateUser()
+	u := &models.User{}
+	u.Name = "test2"
+	u.Email = "test2@example.com"
+	u.Password = "testpassword"
+	fmt.Println(u)
+	u.CreateUser()
 
 	// user, _ := models.GetUser(2)
 
@@ -22,11 +22,18 @@ func main() {
 	// t, _ := models.GetTodo(1)
 	// fmt.Println(t)
 
-	user, _ := models.GetUser(2)
-	user.CreateTodo("Second Todo")
+	user, _ := models.GetUser(3)
+	user.CreateTodo("Third Todo")
 
-	todos, _ := models.GetTodos()
+	// todos, _ := models.GetTodos()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	user2, _ := models.GetUser(2)
+	todos, _ := user2.GetTodosByUser()
 	for _, v := range todos {
 		fmt.Println(v)
 	}
+
 }
