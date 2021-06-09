bf92ba0582c6bdb6ef68d190f74b283c72f8b4fa
Chinatsu Kato <cnt.tkhs@gmail.com>
2021-06-09 09:47:29 +0900

    Read todos

--- main.go
+++ main.go
@@ -19,6 +19,14 @@ func main() {
 
 	// user.CreateTodo("First Todo")
 
-	t, _ := models.GetTodo(1)
-	fmt.Println(t)
+	// t, _ := models.GetTodo(1)
+	// fmt.Println(t)
+
+	user, _ := models.GetUser(2)
+	user.CreateTodo("Second Todo")
+
+	todos, _ := models.GetTodos()
+	for _, v := range todos {
+		fmt.Println(v)
+	}
 }
