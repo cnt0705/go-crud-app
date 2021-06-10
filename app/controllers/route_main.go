29002964207e844eb90db4e7520bda8c38f62054
Chinatsu Kato <cnt.tkhs@gmail.com>
2021-06-10 23:53:00 +0900

    Create layout html

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -2,10 +2,8 @@ package controllers
 
 import (
 	"net/http"
-	"text/template"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("app/views/templates/top.html")
-	t.Execute(w, nil)
+	generateHTML(w, "Hello", "layout", "top")
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,9 +1,22 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"text/template"
 	"todo-module/config"
 )
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
 
 func StartMainServer() error {
 	http.HandleFunc("/", top)
