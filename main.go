530ec9e9ecad5d48af25aecd84c9368692d07938
Chinatsu Kato <cnt.tkhs@gmail.com>
2021-06-10 23:10:12 +0900

    Update todo

--- main.go
+++ main.go
@@ -8,32 +8,7 @@ import (
 func main() {
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test2"
-	u.Email = "test2@example.com"
-	u.Password = "testpassword"
-	fmt.Println(u)
-	u.CreateUser()
-
-	// user, _ := models.GetUser(2)
-
-	// user.CreateTodo("First Todo")
-
-	// t, _ := models.GetTodo(1)
-	// fmt.Println(t)
-
-	user, _ := models.GetUser(3)
-	user.CreateTodo("Third Todo")
-
-	// todos, _ := models.GetTodos()
-	// for _, v := range todos {
-	// 	fmt.Println(v)
-	// }
-
-	user2, _ := models.GetUser(2)
-	todos, _ := user2.GetTodosByUser()
-	for _, v := range todos {
-		fmt.Println(v)
-	}
-
+	t, _ := models.GetTodo(1)
+	t.Content = "updated todo"
+	t.UpdateTodo()
 }
