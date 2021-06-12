612ab507fab02e30b29f9e4f636de6d8d2e9dcfd
Chinatsu Kato <cnt.tkhs@gmail.com>
2021-06-12 12:55:33 +0900

    Create user

--- app/controllers/route_auto.go
+++ app/controllers/route_auto.go
@@ -1,9 +1,31 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
+	"todo-module/app/models"
 )
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "public_navbar", "signup")
+	if r.Method == "GET" {
+		generateHTML(w, nil, "layout", "public_navbar", "signup")
+
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+
+		user := models.User{
+			Name:     r.PostFormValue("name"),
+			Email:    r.PostFormValue("email"),
+			Password: r.PostFormValue("password"),
+		}
+
+		if err := user.CreateUser(); err != nil {
+			log.Println(err)
+		}
+
+		http.Redirect(w, r, "/", 302)
+	}
 }
